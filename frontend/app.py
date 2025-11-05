ecf0814ac0d2441fd1899427ca67f20b409da2ae
tecantik <thesalonica.aes@gmail.com>
2025-11-05 20:38:17 +0700

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -3,7 +3,7 @@ import requests
 
 app = Flask(__name__)
 
-API_BASE_URL = 'https://ab0730bq39.execute-api.us-east-1.amazonaws.com/stage1/users'
+API_BASE_URL = 'https://rvqfjwh2lc.execute-api.us-east-1.amazonaws.com/v1'
 
 @app.route('/')
 def index():
